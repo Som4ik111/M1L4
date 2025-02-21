f7c68c4e0f1bcc8acb6e738d5a931873ec9d2f73
Som4ik111 <ospishchev.mark@gmail.com>
2025-02-21 15:18:55 +0500

    Update and rename bot.my to bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import telebot
-    
-# Инициализация бота с использованием его токена
+from bot_logic import help_bot
+from bot_logic import gift_23
+from bot_logic import gift_8
+
 bot = telebot.TeleBot("7808813003:AAHwxaDYLW4NFEwu9tMo3o5TG8mgplO6gOY")
 
-# Обработчик команды '/start' и '/hello'
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
 
-# Обработчик команды '/heh'
+
 @bot.message_handler(commands=['heh'])
 def send_heh(message):
     count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
@@ -16,9 +18,18 @@ def send_heh(message):
     
 
 @bot.message_handler(commands=['help'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+def send_help(message):
     bot.reply_to(message, help_bot())
+
+
+@bot.message_handler(commands=['gift23'])
+def send_gift23(message):
+    bot.reply_to(message, gift_23())
+
+
+@bot.message_handler(commands=['gift8'])
+def send_gift8(message):
+    bot.reply_to(message, gift_8())
 
 # Запуск бота
 bot.polling()
